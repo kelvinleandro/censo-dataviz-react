f8bc1498d26a466f4b72d1ee1c71e7435e8bfef2
kelvinleandro <kelvinleandro5@gmail.com>
2025-11-28 20:20:02 -0300

    feat: create ScatterPlot component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
 import "./App.css";
-import ChoroplethMap from "@/components/CloropethMap";
+import ScatterPlot from "./components/ScatterPlot";
+import { useState } from "react";
+
+const generateData = (numSamples: number) => {
+  const samples = [];
+  for (let i = 0; i < numSamples; i++) {
+    samples.push({
+      age: Math.floor(Math.random() * 100),
+      income: Math.floor(Math.random() * 10000),
+      gender: Math.random() > 0.5 ? "M" : "F",
+    });
+  }
+  return samples;
+};
 
 function App() {
+  const [numSamples, setNumSamples] = useState(5);
+  const [sampleScatter, setSampleScatter] = useState<
+    { age: number; income: number; gender: string }[]
+  >(() => generateData(numSamples));
+
+  const handleSliderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(e.target.value);
+    setNumSamples(newValue);
+    setSampleScatter(generateData(newValue));
+  };
+
   return (
     <div>
-      <ChoroplethMap />
+      <label htmlFor="sampleSlider" style={{ fontWeight: "bold" }}>
+        Samples: {numSamples}
+      </label>
+      <input
+        id="sampleSlider"
+        type="range"
+        min="5"
+        max="15"
+        step="1"
+        value={numSamples}
+        onChange={handleSliderChange}
+        style={{ cursor: "pointer", width: "200px" }}
+      />
+      <ScatterPlot
+        data={sampleScatter}
+        xField="age"
+        yField="income"
+        width={400}
+        height={400}
+      />
     </div>
   );
 }
---
+++ src/types/vega-lite-api.d.ts
@@ -0,0 +1,5 @@
+declare module "vega-lite-api" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const vl: any;
+  export = vl;
+}
