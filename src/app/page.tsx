fa91f291e9056a56f2c88f40c2f222f673f428ee
gabrimoreira <gabrieldevandrade49@gmail.com>
2025-12-30 19:41:42 -0300

    feat: add initial structure for Chapter 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ChapterOne from "@/components/landing/chapter-01";
 import ChapterFour from "@/components/landing/chapter-04";
+import ChapterTwo from "@/components/landing/chapter-02";
 import Hero from "@/components/landing/hero";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div className="bg-background min-h-screen">
       <Hero />
       <ChapterOne />
+      <ChapterTwo/>
       <ChapterFour />
     </div>
   );
