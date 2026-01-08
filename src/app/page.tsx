e587cd886e6323ef302bf3a584367874e2175b0e
gabrimoreira <gabrieldevandrade49@gmail.com>
2026-01-08 20:40:46 -0300

    feat: add Chapter 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import ChapterOne from "@/components/landing/chapter-01";
 import ChapterFour from "@/components/landing/chapter-04";
 import ChapterTwo from "@/components/landing/chapter-02";
 import Hero from "@/components/landing/hero";
-
+import ChapterFive from "@/components/landing/chapter-05";
 const Home = () => {
   return (
     <div className="bg-background min-h-screen">
       <Hero />
-      <ChapterOne />
+      {/*<ChapterOne />*/}
       <ChapterTwo/>
       <ChapterFour />
+      <ChapterFive/>
     </div>
   );
 };
