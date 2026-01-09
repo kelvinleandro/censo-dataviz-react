87d39cba5b8e15255bd1cc984ffc5bafbbe3b3cf
gabrimoreira <gabrieldevandrade49@gmail.com>
2026-01-08 21:23:20 -0300

    feat: fix numeric problem on query aging-index-by-race

--- src/app/api/aging-index-by-race/route.ts
+++ src/app/api/aging-index-by-race/route.ts
@@ -4,12 +4,12 @@ import { query } from "@/lib/db";
 export async function GET() {
   const data = await query(
     `SELECT 
-    cor_raca, 
-    ROUND(AVG(indice_envelhecimento)::numeric, 2) AS indice_medio
-FROM indice_envelhecimento_raca
-WHERE ano = 2022
-GROUP BY cor_raca
-ORDER BY indice_medio;`
+      cor_raca, 
+      ROUND(AVG(indice_envelhecimento)::numeric, 2) AS indice_medio
+     FROM indice_envelhecimento_raca
+     WHERE ano = 2022
+     GROUP BY cor_raca
+     ORDER BY indice_medio;`
   );
   return NextResponse.json(data);
 }
