ab83b7e439327575fac03288fa46400ff78c45e6
gabrimoreira <gabrieldevandrade49@gmail.com>
2026-01-11 20:25:08 -0300

    feat: add text to epilogue

--- src/components/landing/epilogue.tsx
+++ src/components/landing/epilogue.tsx
@@ -11,21 +11,20 @@ const Epilogue = () => {
 
       <div className="lg:max-w-3/4 mx-auto max-w-4/5 lg:w-full space-y-6">
         <p className="text-lg md:text-xl text-muted-foreground leading-relaxed mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Lorem, ipsum dolor sit
-          amet consectetur adipisicing elit. Nobis blanditiis incidunt sed non
-          ducimus excepturi consectetur dignissimos neque voluptatibus natus
-          voluptatem veritatis, saepe sint perspiciatis nam alias fugit quasi
-          debitis.
+          Os dados do Censo 2022 revelam um país em profunda transição
+          demográfica e social. A pirâmide etária, antes caracterizada por uma
+          base larga de jovens, cede espaço para um envelhecimento acelerado,
+          exigindo uma reavaliação urgente de nossas estruturas de saúde e
+          previdência.
         </p>
 
         <p className="text-lg md:text-xl text-muted-foreground leading-relaxed mb-12">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Lorem, ipsum dolor sit
-          amet consectetur adipisicing elit. Nobis blanditiis incidunt sed non
-          ducimus excepturi consectetur dignissimos neque voluptatibus natus
-          voluptatem veritatis, saepe sint perspiciatis nam alias fugit quasi
-          debitis.
+          Ao analisar o Brasil, raça e educação, a desigualdade torna-se visível
+          no mapa. No entanto, este retrato também marca um momento histórico de
+          reconhecimento, tirando da invisibilidade estatística as populações
+          indígenas e quilombolas. O Brasil que emerge dos números é complexo e
+          diferente: uma sociedade que, apesar da diversidade, tem ainda muitas
+          contradições a lidar.
         </p>
       </div>
     </Section>
